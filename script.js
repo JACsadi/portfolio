3570d30c9a58b9b3703102b28c67e3439afe3782
2021331096 <2021331096@student.sust.edu>
2025-07-20 04:28:02 +0600

    sixth

--- script.js
+++ script.js
@@ -3,35 +3,89 @@ const about = document.querySelector(".about");
 const ach_button = document.querySelector("#ach");
 const ach = document.querySelector(".achivements_list");
 const home = document.querySelector(".staring_page");
+const con_button = document.querySelector("#contact");
+const con = document.querySelector(".contact");
+const qu_button = document.querySelector("#quotes");
+const qu = document.querySelector(".quotes");
+
+qu_button.addEventListener("click", () => {
+  console.log("qu button clicked");
+  if (qu.classList.contains("hidden")) {
+    toggle_hidden();
+    toggle_active();
+    qu_button.classList.add("active");
+    qu.classList.remove("hidden");
+  } else {
+    toggle_hidden();
+    toggle_active();
+    home.classList.remove("hidden");
+  }
+});
+
+con_button.addEventListener("click", () => {
+  console.log("con button clicked");
+  if (con.classList.contains("hidden")) {
+    toggle_hidden();
+    toggle_active();
+    con_button.classList.add("active");
+    con.classList.remove("hidden");
+  } else {
+    toggle_hidden();
+    toggle_active();
+    home.classList.remove("hidden");
+  }
+});
+
 about_button.addEventListener("click", () => {
   console.log("About button clicked");
   if (about.classList.contains("hidden")) {
-    // ach.classList.add("hidden");
-    ach_button.classList.remove("active");
+    toggle_hidden();
+    toggle_active();
     about_button.classList.add("active");
-    home.classList.add("hidden");
     about.classList.remove("hidden");
-    ach.classList.add("hidden");
-    ach_button.classList.remove("active");
   } else {
+    toggle_hidden();
+    toggle_active();
     home.classList.remove("hidden");
-    about.classList.add("hidden");
-    about_button.classList.remove("active");
   }
 });
+
 ach_button.addEventListener("click", () => {
   console.log("Achievements button clicked");
   if (ach.classList.contains("hidden")) {
-    // ach.classList.add("hidden");
+    toggle_hidden();
+    toggle_active();
     ach_button.classList.add("active");
-    about_button.classList.remove("active");
-    home.classList.add("hidden");
     ach.classList.remove("hidden");
-    about.classList.add("hidden");
   } else {
+    toggle_hidden();
+    toggle_active();
     home.classList.remove("hidden");
-    ach.classList.add("hidden");
-    ach_button.classList.remove("active");
   }
 });
-function toggle_hidden() {}
+
+document.querySelectorAll(".achivement").forEach((item) => {
+  item.addEventListener("click", () => {
+    // item = achievement.querySelector(".achivement_description");
+    console.log("Achievement clicked");
+    if (item.classList.contains("expanded")) {
+      item.classList.remove("expanded");
+    } else {
+      item.classList.add("expanded");
+    }
+  });
+});
+
+function toggle_hidden() {
+  about.classList.add("hidden");
+  ach.classList.add("hidden");
+  home.classList.add("hidden");
+  con.classList.add("hidden");
+  qu.classList.add("hidden");
+}
+function toggle_active() {
+  about_button.classList.remove("active");
+  ach_button.classList.remove("active");
+  qu_button.classList.remove("active");
+  con_button.classList.remove("active");
+}
