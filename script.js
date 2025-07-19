fd1c9cddc2ee7a8904c0c585428b650b4e4b0f07
2021331096 <2021331096@student.sust.edu>
2025-07-20 03:27:59 +0600

    fifth

--- script.js
+++ script.js
@@ -1,16 +1,37 @@
 const about_button = document.querySelector("#abt");
 const about = document.querySelector(".about");
-const ach_button = document.querySelector("#achivements");
-const ach = document.querySelector(".achivements");
+const ach_button = document.querySelector("#ach");
+const ach = document.querySelector(".achivements_list");
 const home = document.querySelector(".staring_page");
 about_button.addEventListener("click", () => {
   console.log("About button clicked");
   if (about.classList.contains("hidden")) {
     // ach.classList.add("hidden");
+    ach_button.classList.remove("active");
+    about_button.classList.add("active");
     home.classList.add("hidden");
     about.classList.remove("hidden");
+    ach.classList.add("hidden");
+    ach_button.classList.remove("active");
   } else {
     home.classList.remove("hidden");
     about.classList.add("hidden");
+    about_button.classList.remove("active");
   }
 });
+ach_button.addEventListener("click", () => {
+  console.log("Achievements button clicked");
+  if (ach.classList.contains("hidden")) {
+    // ach.classList.add("hidden");
+    ach_button.classList.add("active");
+    about_button.classList.remove("active");
+    home.classList.add("hidden");
+    ach.classList.remove("hidden");
+    about.classList.add("hidden");
+  } else {
+    home.classList.remove("hidden");
+    ach.classList.add("hidden");
+    ach_button.classList.remove("active");
+  }
+});
+function toggle_hidden() {}
